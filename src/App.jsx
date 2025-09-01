8a0856dca1743f7b2e5cec665483b5980ad1c653
JOAO DE ASSIS FORLANO <CG303805X@car.ifsp>
2025-09-01 17:55:42 -0300

    iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Banner from './components/Banner'
+import Topo from './components/Topo'
+
+function App() {
+
+  return (
+    <>
+     <Topo/>
+     <Banner/> 
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import banner from '../../assets/banner.jpg'
+import bannerMobile from '../../assets/banner_mobile.jpg'
+import './Banner.css'
+
+ export default function Banner() {
+        return (
+           <>
+                
+           
+           </>
+        )
+    }
